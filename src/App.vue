3c663f819cfcf20becb490db307b8fe019454fe2
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-12-22 13:58:56 +0100

    Created main App component

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue';
+
+const count = ref(0);
+</script>
+
+<template>
+    <p>{{count}}</p>
+    <button @click="count++">Click me!</button>
+</template>
+
+<style scoped>
+    p {
+        color: red;
+    }
+</style>
