4faf18e552358b25851f42587b95b3e51c0f8167
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-01-16 11:00:37 +0100

    Removed helpers, added auto cube rotation

--- src/App.vue
+++ src/App.vue
@@ -8,21 +8,24 @@
     const scene = new Scene();
 
     const camera = new PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.set(5, 5, 3);
+    camera.position.set(5, 5, 2);
     scene.add(camera);
 
+    // Used for testing
+    /*  
     const cameraHelper = new CameraHelper(camera);
     scene.add(cameraHelper);
 
     const axesHelper = new AxesHelper(5);
-    scene.add(axesHelper);
+    scene.add(axesHelper); 
+    */
 
     const light = new PointLight(0xffffff, 1, 100);
     light.position.set(0, 5, 5);
     scene.add(light);
 
     const cubeGeometry = new BoxGeometry(1, 1, 1);
-	const cubeMaterial = new MeshStandardMaterial({ color: 0x008080 });
+	const cubeMaterial = new MeshStandardMaterial({ color: 0x00ff83 });
 	const cube = new Mesh(cubeGeometry, cubeMaterial);
 	scene.add(cube);
 
@@ -53,6 +56,8 @@
         controls.enableDamping = true;
         controls.enablePan = false;
         controls.enableZoom = false;
+        controls.autoRotate = true;
+        controls.autoRotateSpeed = 5;
 
         animateScene();
 
